aacb9ebe857e8b6ec93fdc4476efc6a4e0a395bd
jeongseoggi <93635959+jeongseoggi@users.noreply.github.com>
2025-08-14 14:41:13 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer = 0;
+    double div = (double)num1 / num2;
+    answer = (int)(div * 1000);
+    
+    return answer;
+}
