cbb798fe049341c1d9814d9a6b48885f9a02dc39
jeongseoggi <93635959+jeongseoggi@users.noreply.github.com>
2025-08-20 20:26:58 +0900

    Update 문자열 다루기 기본.cpp

--- C++/프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
+++ C++/프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.cpp
@@ -10,11 +10,11 @@ bool solution(string s) {
         for (int i = 0; i < s.length(); i++)
         {
             string ss(1, s[i]);
-            try
+            try // 문자열 정수형으로 변환시도
             {
                 int num = stoi(ss);
             }
-            catch (exception e)
+            catch (exception e) // 변환 실패 -> 숫자가 아닌 문자로 판단 false 리턴
             {
                 return false;
             }
