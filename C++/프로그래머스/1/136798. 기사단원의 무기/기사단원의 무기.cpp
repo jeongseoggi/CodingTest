596787241c05903f97d7ab843a3c970867ad750d
jeongseoggi <93635959+jeongseoggi@users.noreply.github.com>
2025-09-03 13:55:13 +0900

    [level 1] Title: 기사단원의 무기, Time: 100.62 ms, Memory: 4.19 MB -BaekjoonHub

--- C++/프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cpp
+++ C++/프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.cpp
@@ -4,28 +4,46 @@
 
 using namespace std;
 
+int measureCount(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+
+    int count = 2;
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if (n % i == 0)
+        {
+            count += 2;
+            if (i * i == n)
+            {
+                count--;
+            }
+        }
+    }
+    return count;
+}
+
+
+
+
+
 int solution(int number, int limit, int power) 
 {
     int answer = 0;
 
     for (int i = 1; i <= number; i++)
     {
-        int value = i == 1 ? 1 : 2;
-
-        for (int j = 2; j <= i / 2; j++)
-        {
-            if (i % j == 0)
-            {
-                value++;
-            }
-        }
+        int value = measureCount(i);
 
         if (value > limit)
         {
             value = power;
         }
+
         answer += value;
     }
-
     return answer;
 }
