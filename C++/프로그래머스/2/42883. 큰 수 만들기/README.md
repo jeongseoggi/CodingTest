df09231a5ca4c6fe7235d029f5231f7974013874
jeongseoggi <93635959+jeongseoggi@users.noreply.github.com>
2025-11-04 09:42:30 +0900

    [level 2] Title: 큰 수 만들기, Time: 3959.41 ms, Memory: 5.45 MB -BaekjoonHub

--- C++/프로그래머스/2/42883. 큰 수 만들기/README.md
+++ C++/프로그래머스/2/42883. 큰 수 만들기/README.md
@@ -1,6 +1,6 @@
 # [level 2] 큰 수 만들기 - 42883 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42883) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42883#qna) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 04일 09:42:25
+2025년 11월 04일 09:42:29
 
 ### 문제 설명
 
--- C++/프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
+++ C++/프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -3,24 +3,7 @@
 
 using namespace std;
 
-string solution(string number, int k) 
-{
-    string answer = number;
-    
-    auto it = answer.begin();
-    
-    while(k > 0)
-    {
-        if(it + 1 == answer.end() || *it < *(it + 1))
-        {
-            answer.erase(it);
-            k--;
-            it = answer.begin();
-            continue;
-        }
-        
-        it++;
-    }
-
+string solution(string number, int k) {
+    string answer = "";
     return answer;
 }
