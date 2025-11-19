ca96625b148a725876ab4c50ffdf0ded4c3280d7
jeongseoggi <93635959+jeongseoggi@users.noreply.github.com>
2025-11-19 09:45:04 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 74.76 ms, Memory: 14.1 MB -BaekjoonHub

---
+++ C++/프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+map<int, int> dfs(vector<vector<int>>& arr)
+{
+	int size = arr[0].size();
+	int half = size / 2;
+
+	vector<pair<int, int>> area = { {0, 0}, {0, size / 2}, {size / 2, 0}, {size / 2, size / 2} };
+	map<int, int> result;
+
+	if (half == 1)
+	{
+		result[arr[0][0]]++;
+		result[arr[0][1]]++;
+		result[arr[1][0]]++;
+		result[arr[1][1]]++;
+
+		return result;
+	}
+
+	for (int i = 0; i < 4; ++i)
+	{
+		pair<int, int> range = area[i];
+
+		vector<vector<int>> v(half, vector<int>(half, 0));
+
+		map<int, int> count;
+
+		for (int y = 0; y < half; ++y)
+		{
+			for (int x = 0; x < half; ++x)
+			{
+				int nx = x + range.second;
+				int ny = y + range.first;
+
+				v[y][x] = arr[ny][nx];
+
+				count[arr[ny][nx]]++;
+			}
+		}
+
+		if (count[0] == (half * half) || count[1] == (half * half))
+		{
+			count[0] = count[0] / (half * half);
+			count[1] = count[1] / (half * half);
+		}
+		else
+		{
+			count = dfs(v);
+		}
+
+		result[0] += count[0];
+		result[1] += count[1];
+	}
+
+	return result;
+}
+
+vector<int> solution(vector<vector<int>> arr) 
+{
+	vector<int> answer;
+	map<int, int> maps;
+	maps = dfs(arr);
+
+	if (maps[0] == 0)
+	{
+		return { 0, 1 };
+	}
+	else if (maps[1] == 0)
+	{
+		return { 1, 0 };
+	}
+
+	return { maps[0], maps[1] };
+}
