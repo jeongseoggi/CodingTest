fe854dd0844ff8c4a9328aca9060633bc192beca
jeongseoggi <93635959+jeongseoggi@users.noreply.github.com>
2025-12-10 10:24:41 +0900

    [level 1] Title: 최소직사각형, Time: 1.71 ms, Memory: 5.49 MB -BaekjoonHub

--- C++/프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
+++ C++/프로그래머스/1/86491. 최소직사각형/최소직사각형.cpp
@@ -3,46 +3,34 @@
 #include <algorithm>
 using namespace std;
 
-bool compare(const int& a, const int& b)
+int solution(vector<vector<int>> sizes) 
 {
-	return a > b;
-}
-
-int solution(vector<vector<int>> sizes) {
-    int answer = 0;
-
+	int answer = 0;
 	vector<int> v;
-
-
-
-	for (int i = 0; i < sizes.size(); i++)
+	for (int i = 0; i < sizes.size(); ++i)
 	{
-		for (int j = 0; j < sizes[i].size(); j++)
+		for (int j = 0; j < sizes[0].size(); ++j)
 		{
 			v.push_back(sizes[i][j]);
 		}
 	}
 
-	sort(v.begin(), v.end(), compare);
+	sort(v.begin(), v.end(), greater<int>());
 
 	int w = v[0];
 	int h = v[v.size() - 1];
-	int num = 1;
+	int count = 1;
 
-	for (int i = 0; i < sizes.size(); i++)
+
+	for (int i = 0; i < sizes.size(); ++i)
 	{
-		if (sizes[i][1] > h)
+		if (sizes[i][1] > h && sizes[i][0] > h)
 		{
-			if (sizes[i][0] > h)
-			{
-				num++;
-				h = v[v.size() - num];
-				i--;
-			}
+			count++;
+			h = v[v.size() - count];
+			i--;
 		}
 	}
 
-	answer = w * h;
-
-    return answer;
+	return w * h;
 }
