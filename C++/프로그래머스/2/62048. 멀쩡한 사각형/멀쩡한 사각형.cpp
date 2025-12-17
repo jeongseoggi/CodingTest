7e08e00fc6db18da995902ecd0e7a8f0817e055f
jeongseoggi <93635959+jeongseoggi@users.noreply.github.com>
2025-12-17 09:17:16 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ C++/프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.cpp
@@ -0,0 +1,19 @@
+using namespace std;
+
+int GCD(int a, int b)
+{
+    if(b == 0)
+    {
+        return a;
+    }
+    
+    return GCD(b, a % b);
+}
+
+
+long long solution(int w,int h) {
+    long long answer = 1;
+    answer = (long long)w * (long long)h - (w + h - GCD(w, h));
+    
+    return answer;
+}
