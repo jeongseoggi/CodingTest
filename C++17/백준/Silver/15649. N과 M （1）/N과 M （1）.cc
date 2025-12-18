8a9e2feb8c354b7ca9afaae3cf0d64071e6d6680
jeongseoggi <93635959+jeongseoggi@users.noreply.github.com>
2025-12-18 09:17:27 +0900

    [Silver III] Title: N과 M (1), Time: 24 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++17/백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N;
+int M;
+vector<int> numbers;
+
+void backTrack(int index, vector<bool>& used, vector<int>& Permutation)
+{
+	if (index == N + 1)
+	{
+		return;
+	}
+
+	if (Permutation.size() == M)
+	{
+		for (int i = 0; i < Permutation.size(); ++i)
+		{
+			cout << Permutation[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+
+	for (int i = 0; i < N; ++i)
+	{
+		if (!used[i])
+		{
+			used[i] = true;
+			Permutation.push_back(numbers[i]);
+			backTrack(index + 1, used, Permutation);
+			used[i] = false;
+			Permutation.pop_back();
+		}
+	}
+}
+
+
+int main()
+{
+	cin >> N >> M;
+
+	numbers.resize(N);
+	for (int i = 0; i < N; ++i)
+	{
+		numbers[i] = i + 1;
+	}
+
+	vector<bool> used(N, false);
+
+	vector<int> Permutation;
+	backTrack(0, used, Permutation);
+
+	return 0;
+}
