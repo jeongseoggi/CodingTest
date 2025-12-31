beda02c06ca7da702e91b0684142dec81617adab
jeongseoggi <93635959+jeongseoggi@users.noreply.github.com>
2025-12-31 14:41:10 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 58.63 ms, Memory: 15.4 MB -BaekjoonHub

--- C++/프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.cpp
+++ C++/프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.cpp
@@ -2,76 +2,79 @@
 
 using namespace std;
 
-map<int, int> dfs(vector<vector<int>>& arr)
+map<int, int> dfs(vector<vector<int>> arr)
 {
-	int size = arr[0].size();
-	int half = size / 2;
+    int size = arr[0].size();
+    int half = size / 2;
 
-	vector<pair<int, int>> area = { {0, 0}, {0, size / 2}, {size / 2, 0}, {size / 2, size / 2} };
-	map<int, int> result;
+    vector<pair<int, int>> area = { {0,0}, {0, size / 2}, {size / 2, 0}, {size / 2, size / 2} };
 
-	if (half == 1)
-	{
-		result[arr[0][0]]++;
-		result[arr[0][1]]++;
-		result[arr[1][0]]++;
-		result[arr[1][1]]++;
+    map<int, int> result;
 
-		return result;
-	}
+    if (half == 1)
+    {
+        result[arr[0][0]]++;
+        result[arr[0][1]]++;
+        result[arr[1][0]]++;
+        result[arr[1][1]]++;
 
-	for (int i = 0; i < 4; ++i)
-	{
-		pair<int, int> range = area[i];
+        return result;
+    }
 
-		vector<vector<int>> v(half, vector<int>(half, 0));
 
-		map<int, int> count;
+    for (int i = 0; i < area.size(); ++i)
+    {
+        pair<int, int> range = area[i];
 
-		for (int y = 0; y < half; ++y)
-		{
-			for (int x = 0; x < half; ++x)
-			{
-				int nx = x + range.second;
-				int ny = y + range.first;
+        vector<vector<int>> v(half, vector<int>(half, 0));
+        map<int, int> count;
 
-				v[y][x] = arr[ny][nx];
+        for (int j = 0; j < half; ++j)
+        {
+            for (int k = 0; k < half; ++k)
+            {
+                int curX = k + range.second;
+                int curY = j + range.first;
 
-				count[arr[ny][nx]]++;
-			}
-		}
+                v[j][k] = arr[curX][curY];
+                count[arr[curX][curY]]++;
+            }
+        }
 
-		if (count[0] == (half * half) || count[1] == (half * half))
-		{
-			count[0] = count[0] / (half * half);
-			count[1] = count[1] / (half * half);
-		}
-		else
-		{
-			count = dfs(v);
-		}
+        if (count[0] == (half * half) || count[1] == (half * half))
+        {
+            count[0] = count[0] / (half * half);
+            count[1] = count[1] / (half * half);
+        }
+        else
+        {
+            count = dfs(v);
+        }
 
-		result[0] += count[0];
-		result[1] += count[1];
-	}
 
-	return result;
+        result[0] += count[0];
+        result[1] += count[1];
+    }
+
+    return result;
 }
 
-vector<int> solution(vector<vector<int>> arr) 
+
+vector<int> solution(vector<vector<int>> arr)
 {
-	vector<int> answer;
-	map<int, int> maps;
-	maps = dfs(arr);
+    vector<int> answer;
 
-	if (maps[0] == 0)
-	{
-		return { 0, 1 };
-	}
-	else if (maps[1] == 0)
-	{
-		return { 1, 0 };
-	}
+    map<int, int> maps;
 
-	return { maps[0], maps[1] };
+    maps = dfs(arr);
+
+    if (maps[0] == 0)
+    {
+        return { 0, 1 };
+    }
+    else if (maps[1] == 0)
+    {
+        return { 1, 0 };
+    }
+    return {maps[0], maps[1]};
 }
