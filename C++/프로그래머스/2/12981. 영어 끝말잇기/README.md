2815ca70a0694c990330702cf7508d10391ef39d
jeongseoggi <93635959+jeongseoggi@users.noreply.github.com>
2026-01-07 09:59:14 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.03 ms, Memory: 4.16 MB -BaekjoonHub

--- C++/프로그래머스/2/12981. 영어 끝말잇기/README.md
+++ C++/프로그래머스/2/12981. 영어 끝말잇기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 영어 끝말잇기 - 12981 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12981) 
 
 ### 성능 요약
 
-메모리: 4.04 MB, 시간: 0.05 ms
+메모리: 4.16 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 01월 07일 09:52:34
+2026년 01월 07일 09:59:12
 
 ### 문제 설명
 
--- C++/프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
+++ C++/프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
@@ -2,57 +2,87 @@
 
 using namespace std;
 
+// vector<int> solution(int n, vector<string> words) 
+// {
+//     vector<int> answer;
+//     set<string> sets;
+
+//     int count = 0;
+//     for (int i = 0; i < words.size(); ++i)
+//     {
+//         count = i % n;
+
+//         if (i == 0)
+//         {
+//             sets.insert(words[i]);
+//             continue;
+//         }
+
+//         if (sets.find(words[i]) != sets.end())
+//         {
+//             answer.push_back(count + 1);
+//             answer.push_back((i / n) + 1);
+
+//             break;
+//         }
+//         else if (words[i - 1].size() == 1)
+//         {
+//             answer.push_back(count + 1);
+//             answer.push_back((i / n) + 1);
+//             break;
+//         }
+//         else
+//         {
+//             string lastWord = words[i - 1];
+//             string curWord = words[i];
+
+//             if (lastWord[lastWord.size() - 1] != curWord[0])
+//             {
+//                 answer.push_back(count + 1);
+//                 answer.push_back((i / n) + 1);
+//                 break;
+//             }
+//         }
+      
+
+//         sets.insert(words[i]);
+//     }
+
+//     if (answer.size() == 0)
+//     {
+//         answer.push_back(0);
+//         answer.push_back(0);
+//     }
+
+//     return answer;
+// }
+
+// 좀 더 최적화된 방법
 vector<int> solution(int n, vector<string> words) 
 {
     vector<int> answer;
-    set<string> sets;
+    unordered_set<string> sets;
 
-    int count = 0;
-    for (int i = 0; i < words.size(); ++i)
+    sets.insert(words[0]);
+
+    for (int i = 1; i < words.size(); ++i)
     {
-        count = i % n;
+        string lastWord = words[i - 1];
+        string curWord = words[i];
 
-        if (i == 0)
+        if (lastWord.back() != curWord.front())
         {
-            sets.insert(words[i]);
-            continue;
+            return { i % n + 1, i / n + 1 };
         }
 
-        if (sets.find(words[i]) != sets.end())
+        if (sets.find(curWord) != sets.end())
         {
-            answer.push_back(count + 1);
-            answer.push_back((i / n) + 1);
-
-            break;
+            return { i % n + 1, i / n + 1 };
         }
-        else if (words[i - 1].size() == 1)
-        {
-            answer.push_back(count + 1);
-            answer.push_back((i / n) + 1);
-            break;
-        }
-        else
-        {
-            string lastWord = words[i - 1];
-            string curWord = words[i];
 
-            if (lastWord[lastWord.size() - 1] != curWord[0])
-            {
-                answer.push_back(count + 1);
-                answer.push_back((i / n) + 1);
-                break;
-            }
-        }
-      
-
-        sets.insert(words[i]);
+        sets.insert(curWord);
     }
 
-    if (answer.size() == 0)
-    {
-        answer.push_back(0);
-        answer.push_back(0);
-    }
-
-    return answer;
+    return { 0, 0 };
 }
+
