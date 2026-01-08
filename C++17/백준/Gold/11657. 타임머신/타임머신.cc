a3febcd2f9614a04ccb7fc560af6624ff10116ac
jeongseoggi <93635959+jeongseoggi@users.noreply.github.com>
2026-01-08 09:41:11 +0900

    [Gold IV] Title: 타임머신, Time: 8 ms, Memory: 2332 KB -BaekjoonHub

--- C++17/백준/Gold/11657. 타임머신/타임머신.cc
+++ C++17/백준/Gold/11657. 타임머신/타임머신.cc
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-const long long INF = 1e18; 
 
 struct Edge
 {
@@ -10,62 +9,66 @@ struct Edge
     int cost;
 };
 
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
 
-int main() {
     int N, M;
+
     cin >> N >> M;
 
     vector<Edge> edges;
-    for (int i = 0; i < M; i++) {
-        int a, b, c;
-        cin >> a >> b >> c;
-        // 문제에서 주어진 대로 방향성 있는 간선 추가
-        edges.push_back({a, b, c});
+
+    for(int i = 0; i < M; ++i)
+    {
+        int from, to, cost;
+
+        cin >> from >> to >> cost;
+
+        edges.push_back({from, to, cost});
     }
 
-    // 거리 테이블 초기화
-    vector<long long> dist(N + 1, INF);
-    dist[1] = 0; // 1번 도시에서 출발
+    vector<long long> dist(N + 1, INT_MAX);
+    dist[1] = 0;
 
-    // 벨만-포드 알고리즘 수행
-    // N-1번의 반복 + 1번의 사이클 확인 = 총 N번 반복
-    bool hasNegativeCycle = false;
+    bool HasLoop = false;
 
-    for (int i = 1; i <= N; i++) 
+    for(int i = 1; i <= N; ++i)
     {
-        for (int j = 0; j < M; j++) 
+        for(int j = 0; j < M; ++j)
         {
             int cur = edges[j].from;
             int next = edges[j].to;
             int cost = edges[j].cost;
 
-            // 현재 간선의 출발지가 방문 불가능한 곳(INF)이면 패스
-            if (dist[cur] == INF) continue;
+            if(dist[cur] == INT_MAX)
+            {
+                continue;
+            }
 
-            // 최단 거리 갱신 로직 (Relaxation)
             if (dist[next] > dist[cur] + cost) 
             {
                 dist[next] = dist[cur] + cost;
 
                 // N번째 라운드에서도 값이 갱신된다면 음수 사이클 존재
-                if (i == N)
-                {
-                    hasNegativeCycle = true;
+                if (i == N) {
+                    HasLoop = true;
                 }
             }
         }
     }
+    
 
-    if (hasNegativeCycle) 
+    if(HasLoop)
     {
-        cout << -1 << "\n";
-    } 
-    else 
+        cout << "-1" << "\n";
+    }
+    else
     {
-        // 2번 도시부터 N번 도시까지의 최단 거리 출력
-        for (int i = 2; i <= N; i++) 
+        for(int i = 2; i < dist.size(); ++i)
         {
-            if (dist[i] == INF) 
+           if (dist[i] == INT_MAX) 
             {
                 cout << -1 << "\n"; // 도달할 수 없는 경우
             } 
@@ -74,5 +77,8 @@ int main() {
                 cout << dist[i] << "\n";
             }
         }
+
     }
+
+    return 0;
 }
