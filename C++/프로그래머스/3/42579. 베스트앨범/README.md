944fd5fd06d31c0f720b7763088d9edcd28c1286
jeongseoggi <93635959+jeongseoggi@users.noreply.github.com>
2026-01-14 14:36:57 +0900

    [level 3] Title: 베스트앨범, Time: 0.05 ms, Memory: 4.2 MB -BaekjoonHub

--- C++/프로그래머스/3/42579. 베스트앨범/README.md
+++ C++/프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
 
 ### 성능 요약
 
-메모리: 4.21 MB, 시간: 0.04 ms
+메모리: 4.2 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 12월 04일 11:34:56
+2026년 01월 14일 14:36:54
 
 ### 문제 설명
 
--- C++/프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
+++ C++/프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
@@ -2,67 +2,66 @@
 
 using namespace std;
 
-bool Compare(pair<int, int> a, pair<int, int> b)
+bool ComparePairint(pair<int, int> a, pair<int, int> b)
 {
-	return a.second > b.second;
+    return a.second > b.second;
 }
 
-vector<int> solution(vector<string> genres, vector<int> plays) {
-	vector<int> answer;
-
-	unordered_map<string, vector<pair<int, int>>> albumMaps;
-	map<string, int> countMap;
-
-	for (int i = 0; i < genres.size(); ++i)
-	{
-		if (albumMaps[genres[i]].size() == 0)
-		{
-			vector<pair<int, int>> v;
-			v.push_back({ i, plays[i] });
-
-			albumMaps[genres[i]] = v;
-		}
-		else
-		{
-			albumMaps[genres[i]].push_back({ i, plays[i] });
-		}
-		countMap[genres[i]] += plays[i];
-	}
-
-	vector<pair<string, int>> vec(countMap.begin(), countMap.end());
-
-	sort(vec.begin(), vec.end(), [](pair<string, int> a, pair<string, int> b) {
-		return a.second > b.second;
-		});
-
-	for (auto it = albumMaps.begin(); it != albumMaps.end(); ++it)
-	{
-		vector<pair<int, int>> temp(it->second.begin(), it->second.end());
-		sort(temp.begin(), temp.end(), Compare);
-
-		albumMaps[it->first] = temp;
-	}
-
-
-
-
-
-	for (auto it = vec.begin(); it != vec.end(); ++it)
-	{
-		auto findMap = albumMaps[it->first];
-		int count = 2;
-		for (auto it = findMap.begin(); it != findMap.end(); ++it)
-		{
-			if (count == 0)
-			{
-				break;
-			}
-
-			answer.push_back(it->first);
-			--count;
-		}
-	}
-
-	return answer;
+bool ComparePairstring(pair<string,int> a, pair<string,int> b)
+{
+    return a.second > b.second;
 }
 
+
+vector<int> solution(vector<string> genres, vector<int> plays) 
+{
+    vector<int> answer;
+
+    unordered_map<string, vector<pair<int, int>>> albums;
+    unordered_map<string, int> count;
+
+    for (int i = 0; i < genres.size(); ++i)
+    {
+        albums[genres[i]].push_back({ i,plays[i] });
+        count[genres[i]] += plays[i];
+    }
+
+
+   /* vector<pair<string, vector<pair<int, int>>>>v(albums.begin(), albums.end());
+
+    for (int i = 0; i < v.size(); ++i)
+    {
+        sort(v[i].second.begin(), v[i].second.end(), ComparePairint);
+    }*/
+
+    vector<pair<string, int>> countVec(count.begin(), count.end());
+    for (int i = 0; i < countVec.size(); ++i)
+    {
+        sort(countVec.begin(), countVec.end(), ComparePairstring);
+    }
+    
+
+    for (int i = 0; i < countVec.size(); ++i)
+    {
+        string temp = countVec[i].first; // "pop"
+
+        vector<pair<int, int>> tempVec = albums[temp];
+
+        for (int j = 0; j < 2; ++j)
+        {
+            auto it = max_element(tempVec.begin(), tempVec.end(), [](const pair<int, int>& a, const pair<int, int>& b)
+                {
+                    return a.second < b.second;
+                });
+
+            if (it != tempVec.end())
+            {
+                answer.push_back(it->first);
+                tempVec.erase(it);
+            }
+        }
+        
+    }
+  
+    return answer;
+}
