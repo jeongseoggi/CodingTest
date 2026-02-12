4194bc46d5937925a593533794f7462457503cb6
jeongseoggi <93635959+jeongseoggi@users.noreply.github.com>
2026-02-12 09:36:03 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 4.43 MB -BaekjoonHub

--- C++/프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
+++ C++/프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
@@ -3,36 +3,34 @@
 
 using namespace std;
 
-string solution(string s)
+string solution(string s) 
 {
     string answer = "";
     bool isFirst = true;
 
-    for (int i = 0; i <= s.size(); ++i)
+
+    for (int i = 0; i < s.size(); ++i)
     {
         if (isFirst && isalpha(s[i]))
         {
             s[i] = toupper(s[i]);
             isFirst = false;
         }
-        else if (!isFirst)
-        {
-            s[i] = tolower(s[i]);
-        }
-        else
+        else if (!isalpha(s[i]))
         {
             isFirst = false;
         }
+        else if (!isFirst && isupper(s[i]))
+        {
+            s[i] = tolower(s[i]);
+        }
 
-        if (isspace(s[i]))
+
+        if (s[i] == ' ')
         {
             isFirst = true;
         }
     }
 
-    answer = s;
-
-
-    return answer;
+    return s;
 }
-
