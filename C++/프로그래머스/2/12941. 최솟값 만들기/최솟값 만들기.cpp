1103df8e7e0f599b179c10d9ab0f1fa9ec0ae41e
jeongseoggi <93635959+jeongseoggi@users.noreply.github.com>
2026-02-20 09:04:56 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.08 ms, Memory: 3.92 MB -BaekjoonHub

--- C++/프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.cpp
+++ C++/프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
-
 using namespace std;
+
 
 int solution(vector<int> A, vector<int> B)
 {
@@ -12,7 +12,7 @@ int solution(vector<int> A, vector<int> B)
 
     for (int i = 0; i < A.size(); ++i)
     {
-        answer += (A[i] * B[i]);
+        answer += A[i] * B[i];
     }
 
     return answer;
