1cd415418fe0eb8d1b2dc10ce712c848662a0104
jeongseoggi <93635959+jeongseoggi@users.noreply.github.com>
2026-02-26 09:17:38 +0900

    [level 2] Title: 숫자의 표현, Time: 0.05 ms, Memory: 3.95 MB -BaekjoonHub

--- C++/프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
+++ C++/프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
@@ -1,46 +1,38 @@
-#include <bits/stdc++.h>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 int solution(int n) 
 {
-    int answer = 1;
+    int answer = 0;
 
-    vector<int> v;
+    int total = 0;
+    int start = 1;
 
-    for (int i = 1; i <= n; ++i)
+    while (start != n)
     {
-        v.push_back(i);
+        int startCount = start;
+        while (true)
+        {
+            total += startCount;
+            if (total == n)
+            {
+                answer++;
+                break;
+            }
+            else if (total > n)
+            {
+                break;
+            }
+
+            startCount++;
+        }
+        total = 0;
+        start++;
     }
 
-  
 
-    int startIndex = 0;
-    int endIndex = 0;
 
-    for (int i = 0; i < v.size(); ++i)
-    {
-        int value = accumulate(v.begin() + startIndex, v.begin() + endIndex, 0);
-
-        if (value == n)
-        {
-            answer++;
-            startIndex++;
-            continue;
-        }
-
-        if (value > n)
-        {
-            startIndex++;
-            continue;
-        }
-
-        if (value < n)
-        {
-            endIndex++;
-            continue;
-        }
-    }
-
-    return answer;
+    return answer + 1;
 }
