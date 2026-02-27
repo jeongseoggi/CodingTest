4d082716644c6d4bb216f9c506e0d40f626aaa6e
jeongseoggi <93635959+jeongseoggi@users.noreply.github.com>
2026-02-27 09:09:38 +0900

    [level 2] Title: 짝지어 제거하기, Time: 6.06 ms, Memory: 6.43 MB -BaekjoonHub

--- C++/프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
+++ C++/프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
@@ -1,39 +1,48 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int solution(string s)
 {
-    int answer = 1;
+    int answer = 0;
 
     stack<char> st;
 
     st.push(s[0]);
 
-    for (int i = 1; i < s.size(); ++i)
+    int index = 1;
+
+    while (true)
     {
+        if (index >= s.size()) break;
+
         if (st.empty())
         {
-            st.push(s[i]);
+            st.push(s[index]);
+            index++;
             continue;
         }
-        
-        char c = st.top();
 
-        if (c == s[i])
+        char tempC = st.top();
+
+        if (s[index] == tempC)
         {
             st.pop();
         }
         else
         {
-            st.push(s[i]);
+            st.push(s[index]);
         }
+
+        index++;
+
     }
 
-    if (!st.empty())
+    if (st.empty())
+    {
+        return 1;
+    }
+    else
     {
         return 0;
     }
-
-    return answer;
 }
