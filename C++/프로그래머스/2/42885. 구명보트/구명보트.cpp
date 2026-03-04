78757d81d37b7b3086a493b914ae2d97996037d7
jeongseoggi <93635959+jeongseoggi@users.noreply.github.com>
2026-03-04 09:10:00 +0900

    [level 2] Title: 구명보트, Time: 1.78 ms, Memory: 4.72 MB -BaekjoonHub

--- C++/프로그래머스/2/42885. 구명보트/구명보트.cpp
+++ C++/프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -2,19 +2,18 @@
 
 using namespace std;
 
-int solution(vector<int> people, int limit) 
+int solution(vector<int> people, int limit)
 {
     int answer = 0;
 
-    sort(people.begin(), people.end()); // 무게 순 정렬
+    sort(people.begin(), people.end());
 
     int start = 0;
     int end = people.size() - 1;
 
-    // 투 포인터
     while (start <= end)
     {
-        if (people[start] + people[end] <= limit) // 두 무게가 limit가 넘지 않는 경우 두 사람 보트에 탑승
+        if (people[start] + people[end] <= limit)
         {
             start++;
             end--;
