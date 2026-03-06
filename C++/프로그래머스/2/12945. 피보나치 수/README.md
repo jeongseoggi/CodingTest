1684ef8f3bbe86db6b395a98f8927aa3e8fcccc8
jeongseoggi <93635959+jeongseoggi@users.noreply.github.com>
2026-03-06 09:05:42 +0900

    [level 2] Title: 피보나치 수, Time: 1.29 ms, Memory: 4.26 MB -BaekjoonHub

--- C++/프로그래머스/2/12945. 피보나치 수/README.md
+++ C++/프로그래머스/2/12945. 피보나치 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피보나치 수 - 12945 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12945#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12945) 
 
 ### 성능 요약
 
-메모리: 4.27 MB, 시간: 1.30 ms
+메모리: 4.26 MB, 시간: 1.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 22일 09:37:37
+2026년 03월 06일 09:05:39
 
 ### 문제 설명
 
--- C++/프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
+++ C++/프로그래머스/2/12945. 피보나치 수/피보나치 수.cpp
@@ -1,24 +1,21 @@
-#include <string>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-unsigned long long solution(int n)
+long long solution(int n)
 {
-    unsigned long long answer = 0;
+    long long answer = 0;
 
-    vector<unsigned long long> pibo;
-    pibo.push_back(0);
-    pibo.push_back(1);
+    vector<long long> vec;
+    vec.push_back(0);
+    vec.push_back(1);
 
-    for (int i = 2; i <= n; i++)
+    for (int i = 2; i <= n; ++i)
     {
-        pibo.push_back((pibo[i - 2] + pibo[i - 1]) % 1234567);
+        vec.push_back((vec[i - 2] + vec[i - 1]) % 1234567);
     }
 
-    answer = pibo[n] % 1234567;
-
+    answer = vec[n] % 1234567;
 
     return answer;
 }
-
